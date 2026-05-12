6fce37a839ea09318b15410e004c9a430423e974
juanpablo2005-alt <juanpablogarciataimbud@gmail.com>
2026-05-12 14:30:00 -0500

    feat: add BCryptPasswordEncoder bean and PasswordEncoderPort

---
+++ src/main/java/com/petmanagement/application/port/output/PasswordEncoderPort.java
@@ -0,0 +1,6 @@
+package com.petmanagement.application.port.output;
+
+public interface PasswordEncoderPort {
+    String encode(String rawPassword);
+    boolean matches(String rawPassword, String encodedPassword);
+}
